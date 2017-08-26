bb846ccca0b3262880a7fa041c571d25420c41a3
Hitoshi Yunoki <yunoki@porhy.com>
2017-08-27 02:21:57 +0900

    d

--- KRProgressHUD/Classes/InnerKRProgressHUD.swift
+++ KRProgressHUD/Classes/InnerKRProgressHUD.swift
@@ -9,7 +9,7 @@ import UIKit
 
 fileprivate let fadeTime = Double(0.2)
 fileprivate let hudSize = CGSize(width: 150, height: 110)
-fileprivate let simpleHUDSize = CGSize(width: 100, height: 100)
+fileprivate let simpleHUDSize = CGSize(width: 130, height: 130)
 fileprivate let iconViewSize = CGSize(width: 91, height: 91)
 fileprivate let iconViewCenter = CGPoint(x: hudSize.width/2, y: 40)
 fileprivate let simpleHUDIconViewCenter = CGPoint(x: simpleHUDSize.width/2, y: 50)
@@ -28,7 +28,7 @@ extension KRProgressHUD {
       hudView.frame.size = hudSize
       hudView.center = viewAppearance.viewCenterPosition
       hudView.backgroundColor = .white
-      hudView.layer.cornerRadius = 30
+      hudView.layer.cornerRadius = 65
       hudView.autoresizingMask = [.flexibleTopMargin, .flexibleBottomMargin,
                                   .flexibleLeftMargin, .flexibleRightMargin]
 
